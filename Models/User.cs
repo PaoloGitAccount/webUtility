7831553ae87be3ec837544659fd20c49c96ae275
Paolo <zilofi.yahoo.it>
2021-06-19 23:42:39 +0100

    Initial commit

---
+++ Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webUtility.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime DateOfBirth { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime CreatedTime { get; set; }
+
+
+        //{
+        //    get => DateTime.Now;   
+        //    set { this.CreatedTime = value; }
+        //}
+    }
+}
